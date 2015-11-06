0707294cdcec4147217a1ebf450af7445855be89
Haojian Wu <hokein.wu@gmail.com>
2015-11-06 11:57:28 +0800

    Run test through `npm test`.

--- test/test.js
+++ test/test.js
@@ -1,32 +1,33 @@
 var minizip = require('../main');
 var fs = require('fs');
 var assert = require('assert');
+var path = require('path');
 
 describe('node-minizip API', function() {
   it('test valid zip file', function(done) {
-    minizip.zip('./data', './data.zip', function(err) {
+    minizip.zip('test/data', 'test/data.zip', function(err) {
       if (err)
         throw err;
-      minizip.unzip('./data.zip', './tmp', function(err) {
+      minizip.unzip('test/data.zip', 'test/tmp', function(err) {
         if (err)
           throw err;
         assert.notEqual(-1,
-          fs.readFileSync('./tmp/foo.txt').toString().indexOf('foo'));
+          fs.readFileSync('test/tmp/foo.txt').toString().indexOf('foo'));
         assert.notEqual(-1,
-          fs.readFileSync('./tmp/bar.txt').toString().indexOf('bar'));
+          fs.readFileSync('test/tmp/bar.txt').toString().indexOf('bar'));
         done();
       });
     });
   });
   it('test zip non-existent dir', function(done) {
-    minizip.zip('./non-existent', './data.zip', function(err) {
+    minizip.zip('test/non-existent', 'test/data.zip', function(err) {
       if (!err)
         assert.fail(err, 'Expecting error');
       done();
     });
   });
   it('test unzip non-existent dir', function(done) {
-    minizip.unzip('./non-exitent.zip', './tmp', function(err) {
+    minizip.unzip('test/non-exitent.zip', 'test/tmp', function(err) {
       if (!err)
         assert.fail(err, 'Expecting error');
       done();
